a552842adaf50f49fa6e027d91da2c6367ceea46
Tom Meier <ozmeier@yahoo.co.uk>
2009-10-15 09:41:29 +1100

    Updated

--- lib/crm_web_to_lead.rb
+++ lib/crm_web_to_lead.rb
@@ -15,12 +15,13 @@ class AppCallback < FatFreeCRM::Callback::Base
 
     params = controller.params
     params.merge!(params[:ffcrm]).delete(:ffcrm) if (controller.request.xhr? || controller.request.headers["CONTENT_TYPE"] == 'text/xml') && params[:ffcrm]
+
     if controller.request.post? && !params[:authorization].blank? && !params[:token].blank?
       user = User.find_by_password_hash_and_password_salt(params[:authorization], params[:token])
 
       # Implant @current_user so that :require_user filter becomes a noop.
       controller.instance_variable_set("@current_user", user)
-      controller.logger.info(">>> web-to-lead: creating lead for user " + user.inspect) if controller.logger
+      controller.logger.info(">>> web-to-lead: creating lead for user " + user.inspect + "\nParameters:\n#{params.inspect}") if controller.logger
     end
   end
 
